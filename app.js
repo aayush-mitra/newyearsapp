b0e845c7c82213456d51bef3d401366c91f5f3a1
EpicodeCFK <epicodecfk@gmail.com>
2019-12-30 20:19:45 -0600

    Version 1.0.4

--- app.js
+++ app.js
@@ -52,12 +52,12 @@ function timeEvent() {
 
 
 function alertMe() {
-  let huh = prompt("Enter Starting Time:      ex. 60:00");
-  $('#thing').innerHTML = huh;
+
   let interval = setInterval(timeEvent, 1000);
 }
 
-
+let thetime = prompt("Starting Time: ");
+$('#thing').innerHTML = thetime;
 
 
 $('button').onclick = alertMe;
